c94175e231c753dc61bd5a911fe1bb845463e6ac
qutti96 <qutti96@gmail.com>
2020-07-01 16:10:32 +0900

    設定ファイル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,14 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap',
+      },
+    ],
   },
   /*
    ** Global CSS
@@ -63,6 +70,9 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {},
+  router: {
+    base: '/',
+  },
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
